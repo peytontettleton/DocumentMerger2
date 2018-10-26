c8a01e31568cfae60b08a23427f6cfc8cf410010
Peyton Tettleton <peytontettleton@me.com>
2018-10-25 19:44:40 -0500

    Creation

---
+++ Document_Merger_2/Document_Merger_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Document_Merger_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                foreach (string arg in args)
+                {
+                    Console.WriteLine(arg);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No arguments provided");
+            }
+            if (args.Length < 3)
+            {
+                Console.WriteLine("DocumentMerger2 <input_file_1> <input_file_2> ... <input_file_n> <output_file>");
+                Console.WriteLine("Supply a list of text files to merge followed by the name of the resulting merged text file as command line arguments.");
+            }
+        }
+    }
+}
